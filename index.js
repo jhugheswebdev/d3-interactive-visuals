0ad82ba641c1e69aca7582f396dedcf17c77be1f
jhughes <jhugheswebdev@gmail.com>
2018-06-13 14:38:55 -0400

    Multivalue map of attrs

--- index.js
+++ index.js
@@ -12,17 +12,10 @@ var dataset = [ 25, 7, 5, 26, 11, 8, 24, 35, 19, 93, 23, 4, 76, 65, 34, 8, 23, 7
         .data(dataset)
         .enter()
         .append("rect")
-        .attr("x", function(d, i) {
-          return i * (width / dataset.length);
-        })
-        .attr("y", function(d) {
-          return height - d;
-        })
-        .attr("width", width / dataset.length - barPadding)
-        .attr("height", function(d) {
-          return d * 4;
-        })
-        .attr("fill", function(d) {
-          return "rgb(0, 0, " + (d * 10) + ")";
+        .attr({
+          x: function(d, i) { return i * (width / dataset.length); },
+          y: function(d) { return height - (d * 4); },
+          width: width / dataset.length - barPadding,
+          height: function(d) { return d * 4; },
+          fill: function(d) { return "rgb(0, 0, " + (d * 10) + ")"}
         });
-        
