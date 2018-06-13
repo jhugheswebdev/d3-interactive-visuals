036b2f26786151976602ce2d55ab95b58d723f77
jhughes <jhugheswebdev@gmail.com>
2018-06-13 14:55:15 -0400

    Adds labels to graphs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var dataset = [ 25, 7, 5, 26, 11, 8, 24, 35, 19, 93, 23, 4, 76, 65, 34, 8, 23, 76];
+var dataset = [ 5, 7, 5, 26, 21, 8, 24, 25, 19, 23, 23, 4, 26, 25, 24, 8, 23, 26];
 
     var width = 500;
     var height = 100;
@@ -19,3 +19,19 @@ var dataset = [ 25, 7, 5, 26, 11, 8, 24, 35, 19, 93, 23, 4, 76, 65, 34, 8, 23, 7
           height: function(d) { return d * 4; },
           fill: function(d) { return "rgb(0, 0, " + (d * 10) + ")"}
         });
+
+    svg.selectAll("text")
+        .data(dataset)
+        .enter()
+        .append("text")
+        .text(function(d) {
+          return d;
+        })
+        .attr({
+          x: function(d, i) { return i * (width / dataset.length) + (width / dataset.length) / 2; },
+          y: function(d) { return height - (d * 4) + 14; },
+        })
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "11px")
+        .attr("fill", "white")
+        .attr("text-anchor", "middle");
