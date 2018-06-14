f3d43a4d9dbaf221f5d85cea6cae9398de04149e
jhughes <jhugheswebdev@gmail.com>
2018-06-14 11:16:46 -0400

    changes datasets and renders new graph on click

--- index.js
+++ index.js
@@ -1,50 +1,96 @@
-var dataset = [ 5, 27, 25, 26, 21, 8, 24, 25, 19, 23, 23, 24, 26, 25, 24, 8, 23, 26];
+// /Width and height
+			var w = 600;
+			var h = 250;
+			
+			var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+							11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
 
-    var width = 600;
-    var height = 250;
-    var barPadding = 1;
-    // creates svg element
-    var svg = d3.select("body").append("svg")
-                .attr('width', width)
-                .attr('height', height);
+			var xScale = d3.scale.ordinal()
+							.domain(d3.range(dataset.length))
+							.rangeRoundBands([0, w], 0.05);
 
-    var xScale = d3.scale.ordinal()
-                  .domain(d3.range(dataset.length))
-                  .rangeRoundBands([0, width], 0.05);
+			var yScale = d3.scale.linear()
+							.domain([0, d3.max(dataset)])
+							.range([0, h]);
+			
+			//Create SVG element
+			var svg = d3.select("body")
+						.append("svg")
+						.attr("width", w)
+						.attr("height", h);
 
-    // var yScale = d3.scale.linear()
-    //                   .domain([0, d3.max(dataset, function(d) {return d[1];})])
-    //                   .range([height - padding, padding]);
+			//Create bars
+			svg.selectAll("rect")
+			   .data(dataset)
+			   .enter()
+			   .append("rect")
+			   .attr("x", function(d, i) {
+			   		return xScale(i);
+			   })
+			   .attr("y", function(d) {
+			   		return h - yScale(d);
+			   })
+			   .attr("width", xScale.rangeBand())
+			   .attr("height", function(d) {
+			   		return yScale(d);
+			   })
+			   .attr("fill", function(d) {
+					return "rgb(0, 0, " + (d * 10) + ")";
+			   });
 
-    svg.selectAll("rect")
-        .data(dataset)
-        .enter()
-        .append("rect")
-        .attr({
-          x: function(d, i) { return xScale(i); },
-          y: function(d) { return height - (d * 4); },
-          width: xScale.rangeBand(),
-          height: function(d) { return d * 4; },
-          fill: function(d) { return "rgb(0, 0, " + (d * 10) + ")"}
-        });
+			//Create labels
+			svg.selectAll("text")
+			   .data(dataset)
+			   .enter()
+			   .append("text")
+			   .text(function(d) {
+			   		return d;
+			   })
+			   .attr("text-anchor", "middle")
+			   .attr("x", function(d, i) {
+			   		return xScale(i) + xScale.rangeBand() / 2;
+			   })
+			   .attr("y", function(d) {
+			   		return h - yScale(d) + 14;
+			   })
+			   .attr("font-family", "sans-serif")
+			   .attr("font-size", "11px")
+			   .attr("fill", "white");
 
-        
-    svg.selectAll("text")
-        .data(dataset)
-        .enter()
-        .append("text")
-        .text(function(d) {
-          return d;
-        })
-        .attr({
-          x: function(d, i) { return i * (width / dataset.length) + (width / dataset.length) / 2; },
-          y: function(d) { return height - (d * 4) + 14; },
-        })
-        .attr("font-family", "sans-serif")
-        .attr("font-size", "11px")
-        .attr("fill", "white")
-        .attr("text-anchor", "middle");
 
+d3.select("p")
+         .on("click",function() {
+          dataset = [ 11, 12, 15, 20, 18, 17, 16, 18, 23, 25,
+            5, 10, 13, 19, 21, 25, 22, 18, 15, 13 ];
+          
+          svg.selectAll("rect")
+            .data(dataset)
+            .attr("y", function(d) {
+                      return h - yScale(d);
+            })
+            .attr("height", function(d) {
+                      return yScale(d);
+            })
+            .attr("fill", function(d) {
+              return "rgb(0, 0, " + (d * 10) + ")";
+             });
+
+                //Create labels
+          svg.selectAll("text")
+            .data(dataset)
+            .text(function(d) {
+                return d;
+            })
+            .attr("x", function(d, i) {
+                return xScale(i) + xScale.rangeBand() / 2;
+            })
+            .attr("y", function(d) {
+                return h - yScale(d) + 14;
+            })
+            .attr("font-family", "sans-serif")
+            .attr("font-size", "11px")
+            .attr("fill", "white");
+              });
 
 
 // ==================================================================
